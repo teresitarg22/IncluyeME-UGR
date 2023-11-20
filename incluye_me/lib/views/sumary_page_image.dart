903b9264ed222d93d44b7004b62b57f36c484f60
Julia Cano <juliacanoflor@gmail.com>
2023-11-20 13:12:55 +0100

    Pantalla resumen imagenes

--- incluye_me/lib/views/sumary_page_image.dart
+++ incluye_me/lib/views/sumary_page_image.dart
@@ -2,11 +2,30 @@ import 'package:flutter/material.dart';
 
 class SummaryPageImage extends StatelessWidget {
   final Map<String, Map<String, int>> amount;
+  Map<int, String> numeros = {};
+  String? cantidad = "assets/cero.png";
 
   SummaryPageImage({required this.amount});
 
+  /*@override
+  void initState() {
+    numeros[0] = "assets/cero.png";
+    numeros[1] = "assets/uno.png";
+    numeros[2] = "assets/dos.png";
+    numeros[3] = "assets/tres.png";
+    numeros[4] = "assets/cuatro.png";
+    numeros[5] = "assets/cinco.png";
+  }*/
+
   @override
   Widget build(BuildContext context) {
+    numeros[0] = "assets/cero.png";
+    numeros[1] = "assets/uno.png";
+    numeros[2] = "assets/dos.png";
+    numeros[3] = "assets/tres.png";
+    numeros[4] = "assets/cuatro.png";
+    numeros[5] = "assets/cinco.png";
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Resumen'),
@@ -27,10 +46,26 @@ class SummaryPageImage extends StatelessWidget {
                     subtitle: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: entry.value.entries.map((innerEntry) {
-                        return Text(
-                          '${innerEntry.key}: ${innerEntry.value}',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20),
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Image.asset(
+                                '${innerEntry.key}',
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                            Expanded(
+                              child: Image.asset(
+                                numeros[innerEntry.value]!,
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          ],
                         );
                       }).toList(),
                     ),
