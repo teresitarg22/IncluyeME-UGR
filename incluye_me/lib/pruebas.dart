3d8e9c194d698686424ef56e846a78154784eb5d
unknown <alejandrovialard2002@gmail.com>
2023-12-21 19:05:54 +0000

    fichero pruebas

--- incluye_me/lib/pruebas.dart
+++ incluye_me/lib/pruebas.dart
@@ -4,7 +4,7 @@ import 'package:incluye_me/globals/globals.dart';
 import 'package:incluye_me/model/student.dart';
 
 Future<void> main() async {
-  var contenido = await dbDriver.listaTareasGenerales();
+  var contenido = await dbDriver.getTareaCompletada(669);
   print(contenido);
 
   dbDriver.close();
